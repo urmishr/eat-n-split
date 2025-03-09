98bf07f56340d9421b295ca06d214b39931b7eb0
urmishr <ramani.urmish@gmail.com>
2025-03-09 17:56:27 +0530

    done: Static friend list side bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,74 @@
+const initialFriends = [
+    {
+        id: 118836,
+        name: "Clark",
+        image: "https://i.pravatar.cc/48?u=118836",
+        balance: -7,
+    },
+    {
+        id: 933372,
+        name: "Sarah",
+        image: "https://i.pravatar.cc/48?u=933372",
+        balance: 20,
+    },
+    {
+        id: 499476,
+        name: "Anthony",
+        image: "https://i.pravatar.cc/48?u=499476",
+        balance: 0,
+    },
+];
+
 export default function App() {
     return (
-        <div>
-            <h1>Eat and Split</h1>
-            <p>"Share the Meal, Split the Bill!"</p>
+        <>
+            <Header />
+            <div className="app">
+                <div className="sidebar">
+                    <FriendList />
+                </div>
+            </div>
+        </>
+    );
+}
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="title">Eat and Split</h1>
+            <p className="tagLine">"Share the Meal, Split the Bill!"</p>
         </div>
+    );
+}
+
+function FriendList() {
+    const friends = initialFriends;
+    return (
+        <ul>
+            {friends.map((friend) => (
+                <Friend friend={friend} key={friend.id} />
+            ))}
+        </ul>
+    );
+}
+
+function Friend({ friend }) {
+    return (
+        <li>
+            <img src={friend.image} alt={friend.name} />
+            <h3>{friend.name}</h3>
+            {friend.balance < 0 ? (
+                <p className="red">
+                    You owe {friend.name} ${Math.abs(friend.balance)}
+                </p>
+            ) : friend.balance > 0 ? (
+                <p className="green">
+                    {friend.name} owes you ${friend.balance}
+                </p>
+            ) : (
+                <p>You and {friend.name} are even.</p>
+            )}
+            <button className="button">Select</button>
+        </li>
     );
 }
